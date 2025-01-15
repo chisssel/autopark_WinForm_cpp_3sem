f35e1a996196bf54a207dc3533443f802450d7c9
chisssel <platov.artem.15@gmail.com>
2025-01-15 21:42:21 +0300

    first steps to design and logic

--- autopark_WinForm_cpp_3sem/autopark_WinForm_cpp_3sem/MyForm.cpp
+++ autopark_WinForm_cpp_3sem/autopark_WinForm_cpp_3sem/MyForm.cpp
@@ -1,5 +1,11 @@
 #include "MyForm.h"
 
+
+#include <windows.h>
+#include <sqlite3.h>
+#include <string>
+#include <sstream>
+
 using namespace System;
 using namespace System::Windows::Forms;
 
@@ -8,6 +14,116 @@ using namespace System::Windows::Forms;
 int main(array<String^>^ args) {
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::EnableVisualStyles();
+    sqlite3* db;
+    if (sqlite3_open("transport.db", &db) != SQLITE_OK) {
+        MessageBox::Show("Ошибка открытия базы данных!");
+        return 1;
+    }
 	autoparkWinFormcpp3sem::MyForm form;
 	Application::Run(% form);
+
+    
+
 }
+
+// Функция для выполнения SQL-запросов
+void executeSQL(sqlite3* db, const std::string& sql) {
+    char* errMsg = nullptr;
+    if (sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg) != SQLITE_OK) {
+        MessageBox::Show("Ошибка выполнения SQL-запроса: " + gcnew String(errMsg));
+        sqlite3_free(errMsg);
+    }
+}
+
+// Функция для расчёта стоимости перевозки
+double calculateCost(sqlite3* db, double distance, double weight) {
+    std::string sql = "SELECT cost_per_km, cost_per_kg FROM tariffs WHERE id = 1;";
+    sqlite3_stmt* stmt;
+    double costPerKm = 0, costPerKg = 0;
+
+    if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) == SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+            costPerKm = sqlite3_column_double(stmt, 0);
+            costPerKg = sqlite3_column_double(stmt, 1);
+        }
+        sqlite3_finalize(stmt);
+    }
+
+    return (distance * costPerKm) + (weight * costPerKg);
+}
+
+// Основная форма приложения
+//public ref class MainForm : public Form {
+//private:
+//    sqlite3* db;
+//
+//public:
+//    MainForm() {
+//        InitializeComponent();
+//        sqlite3* db;
+//        // Открытие базы данных
+//        sqlite3_open("transport.db", &db);
+//        if (sqlite3_open("transport.db", &db) != SQLITE_OK) {
+//            MessageBox::Show("Ошибка открытия базы данных!");
+//            Close();
+//        }
+//    }
+//
+//    ~MainForm() {
+//        sqlite3_close(db);
+//    }
+//
+//private:
+//    TextBox^ distanceTextBox;
+//    TextBox^ weightTextBox;
+//    Label^ resultLabel;
+//    Button^ calculateButton;
+//
+//    void InitializeComponent() {
+//        this->Text = "Калькулятор перевозки";
+//        this->Size = System::Drawing::Size(300, 200);
+//
+//        distanceTextBox = gcnew TextBox();
+//        distanceTextBox->Location = System::Drawing::Point(50, 30);
+//        distanceTextBox->Text = "0";
+//
+//        weightTextBox = gcnew TextBox();
+//        weightTextBox->Location = System::Drawing::Point(50, 60);
+//        weightTextBox->Text = "0";
+//
+//        resultLabel = gcnew Label();
+//        resultLabel->Location = System::Drawing::Point(50, 90);
+//        resultLabel->Text = "Результат: ";
+//
+//        calculateButton = gcnew Button();
+//        calculateButton->Location = System::Drawing::Point(50, 120);
+//        calculateButton->Text = "Рассчитать";
+//        calculateButton->Click += gcnew EventHandler(this, &MainForm::OnCalculateClick);
+//
+//        this->Controls->Add(distanceTextBox);
+//        this->Controls->Add(weightTextBox);
+//        this->Controls->Add(resultLabel);
+//        this->Controls->Add(calculateButton);
+//    }
+//
+//    void OnCalculateClick(Object^ sender, EventArgs^ e) {
+//        double distance = Double::Parse(distanceTextBox->Text);
+//        double weight = Double::Parse(weightTextBox->Text);
+//
+//        double totalCost = calculateCost(db, distance, weight);
+//        resultLabel->Text = "Результат: " + totalCost.ToString("C");
+//
+//        // Сохранение результата в базу данных
+//        std::stringstream sql;
+//        sql << "INSERT INTO history (distance, weight, total_cost, calculation_date) VALUES ("
+//            << distance << ", " << weight << ", " << totalCost << ", datetime('now'));";
+//        executeSQL(db, sql.str());
+//    }
+//};
+//
+//[STAThread]
+//int main() {
+//    Application::EnableVisualStyles();
+//    Application::Run(gcnew MainForm());
+//    return 0;
+//}
--- autopark_WinForm_cpp_3sem/autopark_WinForm_cpp_3sem/MyForm.h
+++ autopark_WinForm_cpp_3sem/autopark_WinForm_cpp_3sem/MyForm.h
@@ -42,6 +42,14 @@ namespace autoparkWinFormcpp3sem {
 	private: System::Windows::Forms::RadioButton^ radioButton1;
 	private: System::Windows::Forms::CheckBox^ checkBox;
 	private: System::Windows::Forms::TextBox^ textBox;
+	private: System::Windows::Forms::Label^ welcome;
+	private: System::Windows::Forms::Label^ headWelcome;
+	private: System::Windows::Forms::Label^ login;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBoxLogin;
+	private: System::Windows::Forms::TextBox^ textBoxPass;
+	private: System::Windows::Forms::Button^ Enter;
+
 
 	protected:
 
@@ -65,12 +73,19 @@ namespace autoparkWinFormcpp3sem {
 			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
 			this->checkBox = (gcnew System::Windows::Forms::CheckBox());
 			this->textBox = (gcnew System::Windows::Forms::TextBox());
+			this->welcome = (gcnew System::Windows::Forms::Label());
+			this->headWelcome = (gcnew System::Windows::Forms::Label());
+			this->login = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBoxLogin = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxPass = (gcnew System::Windows::Forms::TextBox());
+			this->Enter = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// label_change
 			// 
 			this->label_change->AutoSize = true;
-			this->label_change->Location = System::Drawing::Point(152, 122);
+			this->label_change->Location = System::Drawing::Point(12, 57);
 			this->label_change->Name = L"label_change";
 			this->label_change->Size = System::Drawing::Size(44, 16);
 			this->label_change->TabIndex = 0;
@@ -79,7 +94,7 @@ namespace autoparkWinFormcpp3sem {
 			// 
 			// btn_main
 			// 
-			this->btn_main->Location = System::Drawing::Point(95, 161);
+			this->btn_main->Location = System::Drawing::Point(12, 104);
 			this->btn_main->Name = L"btn_main";
 			this->btn_main->Size = System::Drawing::Size(200, 62);
 			this->btn_main->TabIndex = 1;
@@ -91,7 +106,7 @@ namespace autoparkWinFormcpp3sem {
 			// 
 			this->radioButton1->AutoSize = true;
 			this->radioButton1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->radioButton1->Location = System::Drawing::Point(214, 276);
+			this->radioButton1->Location = System::Drawing::Point(15, 172);
 			this->radioButton1->Name = L"radioButton1";
 			this->radioButton1->Size = System::Drawing::Size(103, 20);
 			this->radioButton1->TabIndex = 2;
@@ -103,7 +118,7 @@ namespace autoparkWinFormcpp3sem {
 			// checkBox
 			// 
 			this->checkBox->AutoSize = true;
-			this->checkBox->Location = System::Drawing::Point(134, 399);
+			this->checkBox->Location = System::Drawing::Point(15, 198);
 			this->checkBox->Name = L"checkBox";
 			this->checkBox->Size = System::Drawing::Size(95, 20);
 			this->checkBox->TabIndex = 3;
@@ -113,18 +128,94 @@ namespace autoparkWinFormcpp3sem {
 			// 
 			// textBox
 			// 
-			this->textBox->Location = System::Drawing::Point(439, 116);
+			this->textBox->Location = System::Drawing::Point(12, 76);
 			this->textBox->Name = L"textBox";
 			this->textBox->Size = System::Drawing::Size(100, 22);
 			this->textBox->TabIndex = 4;
 			this->textBox->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox_TextChanged);
+			// 
+			// welcome
+			// 
+			this->welcome->AutoSize = true;
+			this->welcome->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->welcome->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->welcome->Location = System::Drawing::Point(299, 130);
+			this->welcome->Name = L"welcome";
+			this->welcome->Size = System::Drawing::Size(204, 36);
+			this->welcome->TabIndex = 5;
+			this->welcome->Text = L"Авторизация";
+			this->welcome->Click += gcnew System::EventHandler(this, &MyForm::label1_Click_1);
+			// 
+			// headWelcome
+			// 
+			this->headWelcome->AutoSize = true;
+			this->headWelcome->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->headWelcome->Location = System::Drawing::Point(36, 15);
+			this->headWelcome->Name = L"headWelcome";
+			this->headWelcome->Size = System::Drawing::Size(752, 42);
+			this->headWelcome->TabIndex = 6;
+			this->headWelcome->Text = L"Рассчитайте стоимость Вашей перевозки";
+			// 
+			// login
+			// 
+			this->login->AutoSize = true;
+			this->login->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->login->Location = System::Drawing::Point(126, 193);
+			this->login->Name = L"login";
+			this->login->Size = System::Drawing::Size(155, 25);
+			this->login->TabIndex = 7;
+			this->login->Text = L"Введите логин:";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(126, 233);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(167, 25);
+			this->label1->TabIndex = 8;
+			this->label1->Text = L"Введите пароль:";
+			// 
+			// textBoxLogin
+			// 
+			this->textBoxLogin->Location = System::Drawing::Point(305, 198);
+			this->textBoxLogin->Name = L"textBoxLogin";
+			this->textBoxLogin->Size = System::Drawing::Size(100, 22);
+			this->textBoxLogin->TabIndex = 9;
+			// 
+			// textBoxPass
+			// 
+			this->textBoxPass->Location = System::Drawing::Point(305, 237);
+			this->textBoxPass->Name = L"textBoxPass";
+			this->textBoxPass->Size = System::Drawing::Size(100, 22);
+			this->textBoxPass->TabIndex = 10;
+			// 
+			// Enter
+			// 
+			this->Enter->Location = System::Drawing::Point(305, 281);
+			this->Enter->Name = L"Enter";
+			this->Enter->Size = System::Drawing::Size(164, 46);
+			this->Enter->TabIndex = 11;
+			this->Enter->Text = L"Войти";
+			this->Enter->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::Control;
-			this->ClientSize = System::Drawing::Size(813, 550);
+			this->ClientSize = System::Drawing::Size(925, 550);
+			this->Controls->Add(this->Enter);
+			this->Controls->Add(this->textBoxPass);
+			this->Controls->Add(this->textBoxLogin);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->login);
+			this->Controls->Add(this->headWelcome);
+			this->Controls->Add(this->welcome);
 			this->Controls->Add(this->textBox);
 			this->Controls->Add(this->checkBox);
 			this->Controls->Add(this->radioButton1);
@@ -132,7 +223,7 @@ namespace autoparkWinFormcpp3sem {
 			this->Controls->Add(this->label_change);
 			this->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
 			this->Name = L"MyForm";
-			this->Text = L"AutoPark";
+			this->Text = L"Рассчет стоимости перевоза груза";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -160,5 +251,10 @@ namespace autoparkWinFormcpp3sem {
 		else if (this->textBox->Text == "Red")
 			this->btn_main->BackColor = Color::Red;
 	}
+private: System::Void label1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
+
+
+
